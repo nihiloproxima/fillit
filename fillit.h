4a8d955e4aa2534036efb71b9de3870e360c5efe
Nathan Plouvier <naplouvi@student.le-101.fr>
2018-10-22 16:55:48 +0200

    ok

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: naplouvi <naplouvi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/18 13:00:30 by ftourret     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/22 12:31:38 by naplouvi    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/22 13:00:06 by naplouvi    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,12 +21,12 @@ typedef struct	s_tetro
 {
 	char		**content;
 	char		*shape;
+	char		id;
 	void		*next;
-	char		letter;
 }				t_tetro;
 
 void			ft_usage(int argc);
 void			ft_error(void);
-int				ft_check_tetrominos(char **tetro);
+int				ft_check_tetrominos(t_tetro *tetro);
 
 #endif
--- ft_check_tetrominos.c
+++ ft_check_tetrominos.c
@@ -6,27 +6,27 @@
 /*   By: naplouvi <naplouvi@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/10/18 16:51:06 by naplouvi     #+#   ##    ##    #+#       */
-/*   Updated: 2018/10/22 12:40:37 by naplouvi    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/10/22 16:06:02 by naplouvi    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	ft_check_tetrominos(t_tetro *tetro)
+int		ft_check_tetrominos(char **tetro)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (tetro->content[i])
+	while (tetro[i])
 	{
 		j = 0;
-		if (ft_lentochar(tetro->content[i], '\n') != 4)
+		if (ft_lentochar(tetro[i], '\n') != 4)
 			ft_error();
-		while (tetro->content[i][j])
+		while (tetro[i][j])
 		{
-			if (tetro->content[i][j] != '.' || tetro->content[i][j] != '#')
+			if (tetro[i][j] != '.' || tetro[i][j] != '#')
 				ft_error();
 			j++;
 		}
@@ -35,23 +35,36 @@ int	ft_check_tetrominos(t_tetro *tetro)
 	return (0);
 }
 
-int	ft_checkmap(char **map)
+char ***map_setup(void)
 {
+	char tetros[26][5][5];
+	int	y;
+	int	i;
+
+	i = 0;
+	while (tetros[i])
+	{
+		y = 0;
+		while (y++ < 5)
+			tetros[i][y][5] = '\0';
+		tetros[i][5] = NULL;
+	}
+}
+
+char	***ft_map(char **map)
+{
+	char	tetros[26][5][5];
 	int		x;
 	int		y;
-	int		i;
-	int		j;
-	int		empty;
-	t_tetro	*tetro;
 
 	y = 0;
-	empty = 4;
 	while (map[y])
 	{
 		x = 0;
 		while (map[y][x])
 		{
-			tetro->content[]
+			if (map[y][0] == '\n')
+				break ;
 		}
 	}
 }
